43264c64a2d5d0384345dd69ca6e382b2a8152e6
Mirbek <42761675+mirbeck@users.noreply.github.com>
2020-07-26 02:13:27 +0200

    Sync directory tree to s3

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,32 +1,50 @@
+#!usr/bin/python
+#-*- coding: utf-8 -*-
+
+""" Webotron: Deploy websites with aws
+
+Webotron automates the process of deploying static websites
+- Configure AWS S3 buckets
+    - Create them
+    - Set them up fo static website hosting
+    - Deploy local files to them
+- Configure DNS with AWS Route 53
+- Configure a Content Delivery Network an SSL with AWS CloudFront
+"""
+
+from pathlib import Path
+import mimetypes
+
 import boto3
+from botocore.exceptions import ClientError
 import click
-from botocore3.execptions import ClientError
+
 
 session = boto3.Session(profile_name = 'pythonAutomation')
 s3 = session. resource("s3")
 
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS"""
     pass
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets"""
     for bucket in s3.buckets.all():
         print(bucket)
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_obects(bucket):
-    "List objects in s3 bucket"
+    """List objects in s3 bucket"""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
 
 @cli.command ('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure s3 bucket"
+    """Create and configure s3 bucket"""
     s3_bucket = None
     try:
         s3_bucket = s3.create_bucket(
@@ -66,5 +84,30 @@ def setup_bucket(bucket):
         }
         )
     return
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        })
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    """Sync contents of PATHNAME to s3 bucket"""
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+    handle_directory(root)
+
 if __name__ == '__main__':
     cli()
--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# automating-aws-with-python
-
-#Simple changes made on 25 of July 2020.
+# Automating AWS with Python
 
 ## 01-webotron
 
-## Webotron a scrpit that will sync a local directory to an s3 bucket, and optionaly config route 53 and CloudFront as well.
+## Webotron a scrpit that will sync a local directory to an s3 bucket, and optionaly config Route 53 and CloudFront as well.
 
 ### Features
 
@@ -13,3 +11,4 @@ Webotron currently has the following features:
 - List bucket
 - List contents of a bucket
 - Create and setup buckets
+- Sync directory tree to bucket
